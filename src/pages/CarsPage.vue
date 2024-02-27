0d19f3a7350082bb98f9ab1de086d8fb48e974a4
Joaquin Chavez <jchav5601@gmail.com>
2024-02-27 09:34:56 -0700

    Created Cars Page

---
+++ src/pages/CarsPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="m-3">Cars</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+setup(){
+return{}
+}
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
