c2c5e0a8e9663985df9f3b0fa03f58991690ec23
Joaquin Chavez <jchav5601@gmail.com>
2024-02-27 12:17:13 -0700

    showing car details

--- src/pages/CarDetailsPage.vue
+++ src/pages/CarDetailsPage.vue
@@ -1,14 +1,31 @@
 <template>
-    <h1>Car Details</h1>
+    <div class="container">
+        <section class="row">
+            <div class="col-12">
+                <h1 class="my-3">Car Details</h1>
+            </div>
+        </section>
+        
+        <section class="row" v-if="car">
+            <div class="col-12">
+                <h2>Make: {{ car.make }} Model: {{ car.model }}</h2>
+                <h3>Year: {{ car.year }}</h3>
+                <h3>Listed by: {{ car.creator.name }}</h3>
+                <img :src="car.imgUrl" :alt="car.make + ' ' + car.model">
+                <p>{{ car.description }}</p>
+            </div>
+        </section>
+    </div>
 </template>
 
 
 <script>
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 import Pop from '../utils/Pop.js';
 import { carsService } from '../services/CarsService.js';
 import { logger } from '../utils/Logger.js';
 import { useRoute } from 'vue-router';
+import { AppState } from '../AppState.js'
 
 export default {
 setup(){
@@ -31,12 +48,16 @@ setup(){
         getCarById()
     })
     
-return{}
+return{
+    car: computed(() => AppState.activeCar)
+}
 }
 }
 </script>
 
 
 <style lang="scss" scoped>
-
+    img {
+        max-height: 40vh;
+    }
 </style>
