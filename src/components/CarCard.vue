f7c0637408501914de770035862dd28056ea0642
Joaquin Chavez <jchav5601@gmail.com>
2024-02-27 11:10:59 -0700

    Abstracted carCard to component

---
+++ src/components/CarCard.vue
@@ -0,0 +1,47 @@
+<template>
+    <img :src="carProp.imgUrl" :alt="carProp.make + ' ' + carProp.model" class="img-fluid" role="button">
+        <div class="car-title">
+            <p class="fs-4 mb-1 ms-3">{{ carProp.make }} {{ carProp.model }}</p>
+        </div>
+</template>
+
+
+<script>
+import { Carousel } from 'bootstrap';
+import { Cars } from '../models/Cars.js';
+
+export default {
+    props: {
+        carProp: {type: Cars, required: true}
+    },
+setup(){
+return{
+    
+}
+}
+}
+</script>
+
+
+<style lang="scss" scoped>
+    img {
+        height: 40vh;
+        width: 40vh;
+        border-radius: 16px;
+        object-fit: cover;
+        object-position: center;
+        box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+        filter: brightness(80%);
+    }
+    img:hover {
+        box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+    }
+    
+    .car-title {
+        position: absolute;
+        bottom: 0;
+        margin: auto;
+        color: white;
+        text-shadow: 1px 1px 3px black;
+    }
+</style>
--- src/pages/CarsPage.vue
+++ src/pages/CarsPage.vue
@@ -8,10 +8,7 @@
         
         <section class="row">
             <div class="col-md-4 car-card mb-5" v-for="car in cars" :key="car.id">
-                <img :src="car.imgUrl" :alt="car.make + ' ' + car.model" class="img-fluid">
-                <div class="car-title">
-                    <p class="fs-4 mb-1 ms-3">{{ car.make }} {{ car.model }}</p>
-                </div>
+                <CarCard :carProp="car"/>
             </div>
         </section>
     </div>
@@ -26,51 +23,36 @@ import { computed, onMounted } from 'vue';
 import Pop from '../utils/Pop.js';
 import { carsService } from '../services/CarsService.js'
 import { AppState } from '../AppState.js'
+import CarCard from '../components/CarCard.vue';
 
 export default {
-setup(){
-    
-    async function getCars() {
-        try {
-            await carsService.getCars()
-        } catch (error) {
-            Pop.error(error)
+    setup() {
+        async function getCars() {
+            try {
+                await carsService.getCars();
+            }
+            catch (error) {
+                Pop.error(error);
+            }
         }
-    }
-    
-    onMounted(() => {
-        console.log('Page is mounted');
-        getCars()
-    })
-    
-return{
-    cars: computed(() => AppState.cars)
-    }
-  }
+        onMounted(() => {
+            console.log('Page is mounted');
+            getCars();
+        });
+        return {
+            cars: computed(() => AppState.cars)
+        };
+    },
+    components: { CarCard }
 }
 </script>
 
 
 <style lang="scss" scoped>
-    img {
-        height: 40vh;
-        width: 40vh;
-        border-radius: 16px;
-        object-fit: cover;
-        object-position: center;
-        box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
-        filter: brightness(80%);
-    }
+
     
     .car-card {
         position: relative;
     }
     
-    .car-title {
-        position: absolute;
-        bottom: 0;
-        margin: auto;
-        color: white;
-        text-shadow: 1px 1px 3px black;
-    }
 </style>
